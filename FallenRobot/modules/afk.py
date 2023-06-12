8ad3b62d5da3f35d143f299f45dee14af50ee802
ᴅᴇᴠᴀɴsʜ-ʙᴏᴛs <125127646+Devansh-Bots@users.noreply.github.com>
2023-06-12 20:17:26 +0530

    Update afk.py

--- FallenRobot/modules/afk.py
+++ FallenRobot/modules/afk.py
@@ -139,8 +139,8 @@ def check_afk(update, context, user_id, fst_name, userc_id):
 
 __help__ = """
 *Away from group*
- ❍ /afk <reason>*:* mark yourself as AFK(away from keyboard).
- ❍ brb <reason>*:* same as the afk command - but not a command.
+ ❉ /afk <reason>*:* mark yourself as AFK(away from keyboard).
+ ❉ brb <reason>*:* same as the afk command - but not a command.
 When marked as AFK, any mentions will be replied to with a message to say you're not available!
 """
 
@@ -160,7 +160,7 @@ dispatcher.add_handler(AFK_REGEX_HANDLER, AFK_GROUP)
 dispatcher.add_handler(NO_AFK_HANDLER, AFK_GROUP)
 dispatcher.add_handler(AFK_REPLY_HANDLER, AFK_REPLY_GROUP)
 
-__mod_name__ = "Aꜰᴋ​"
+__mod_name__ = "𝐀ғᴋ"
 __command_list__ = ["afk"]
 __handlers__ = [
     (AFK_HANDLER, AFK_GROUP),
