073cd31b733147afdff2f7da7349b8ab85ed8d27
ᴅᴇᴠᴀɴsʜ-ʙᴏᴛs <125127646+Devansh-Bots@users.noreply.github.com>
2023-06-13 14:04:01 +0530

    Update blacklist_stickers.py

--- FallenRobot/modules/blacklist_stickers.py
+++ FallenRobot/modules/blacklist_stickers.py
@@ -490,17 +490,17 @@ def __stats__():
 __help__ = """
 Blacklist sticker is used to stop certain stickers. Whenever a sticker is sent, the message will be deleted immediately.
 *NOTE:* Blacklist stickers do not affect the group admin
- ❍ /blsticker*:* See current blacklisted sticker
+ ❉ /blsticker*:* See current blacklisted sticker
 *Only admin:*
- ❍ /addblsticker <sticker link>*:* Add the sticker trigger to the black list. Can be added via reply sticker
- ❍ /unblsticker <sticker link>*:* Remove triggers from blacklist. The same newline logic applies here, so you can delete multiple triggers at once
- ❍ /rmblsticker <sticker link>*:* Same as above
- ❍ /blstickermode <ban/tban/mute/tmute>*:* sets up a default action on what to do if users use blacklisted stickers
+ ❉ /addblsticker <sticker link>*:* Add the sticker trigger to the black list. Can be added via reply sticker
+ ❉ /unblsticker <sticker link>*:* Remove triggers from blacklist. The same newline logic applies here, so you can delete multiple triggers at once
+ ❉ /rmblsticker <sticker link>*:* Same as above
+ ❉ /blstickermode <ban/tban/mute/tmute>*:* sets up a default action on what to do if users use blacklisted stickers
 Note:
- ❍ <sticker link> can be `https://t.me/addstickers/<sticker>` or just `<sticker>` or reply to the sticker message
+ ❉ <sticker link> can be `https://t.me/addstickers/<sticker>` or just `<sticker>` or reply to the sticker message
 """
 
-__mod_name__ = "Bʟ-Sᴛɪᴄᴋ"
+__mod_name__ = "𝐁ʟ-𝐒ᴛɪᴄᴋ"
 
 BLACKLIST_STICKER_HANDLER = DisableAbleCommandHandler(
     "blsticker", blackliststicker, admin_ok=True, run_async=True
