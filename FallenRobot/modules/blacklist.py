ea9b62e1963db8cba9227b0f92b86774bf0def3a
ᴅᴇᴠᴀɴsʜ-ʙᴏᴛs <125127646+Devansh-Bots@users.noreply.github.com>
2023-06-13 14:00:12 +0530

    Update blacklist.py

--- FallenRobot/modules/blacklist.py
+++ FallenRobot/modules/blacklist.py
@@ -444,7 +444,7 @@ def __stats__():
     )
 
 
-__mod_name__ = "Bʟᴀᴄᴋʟɪsᴛ"
+__mod_name__ = "𝐁ʟᴀᴄᴋʟɪsᴛ"
 
 __help__ = """
 
@@ -452,12 +452,12 @@ Blacklists are used to stop certain triggers from being said in a group. Any tim
 
 *NOTE*: Blacklists do not affect group admins.
 
- ❍ /blacklist*:* View the current blacklisted words.
+ ❉ /blacklist*:* View the current blacklisted words.
 
 Admin only:
- ❍ /addblacklist <triggers>*:* Add a trigger to the blacklist. Each line is considered one trigger, so using different lines will allow you to add multiple triggers.
- ❍ /unblacklist <triggers>*:* Remove triggers from the blacklist. Same newline logic applies here, so you can remove multiple triggers at once.
- ❍ /blacklistmode <off/del/warn/ban/kick/mute/tban/tmute>*:* Action to perform when someone sends blacklisted words.
+ ❉ /addblacklist <triggers>*:* Add a trigger to the blacklist. Each line is considered one trigger, so using different lines will allow you to add multiple triggers.
+ ❉ /unblacklist <triggers>*:* Remove triggers from the blacklist. Same newline logic applies here, so you can remove multiple triggers at once.
+ ❉ /blacklistmode <off/del/warn/ban/kick/mute/tban/tmute>*:* Action to perform when someone sends blacklisted words.
 """
 
 BLACKLIST_HANDLER = DisableAbleCommandHandler(
