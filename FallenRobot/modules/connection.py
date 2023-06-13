cddd0224e70659db189eb4ca13ee84ac9b6e8274
ᴅᴇᴠᴀɴsʜ-ʙᴏᴛs <125127646+Devansh-Bots@users.noreply.github.com>
2023-06-13 14:31:17 +0530

    Update connection.py

--- FallenRobot/modules/connection.py
+++ FallenRobot/modules/connection.py
@@ -375,19 +375,19 @@ def connect_button(update, context):
         connect_chat(update, context)
 
 
-__mod_name__ = "Cᴏɴɴᴇᴄᴛ"
+__mod_name__ = "𝐂ᴏɴɴᴇᴄᴛ"
 
 __help__ = """
 Sometimes, you just want to add some notes and filters to a group chat, but you don't want everyone to see; This is where connections come in...
 This allows you to connect to a chat's database, and add things to it without the commands appearing in chat! For obvious reasons, you need to be an admin to add things; but any member in the group can view your data.
 
- ❍ /connect: Connects to chat (Can be done in a group by /connect or /connect <chat id> in PM)
- ❍ /connection: List connected chats
- ❍ /disconnect: Disconnect from a chat
- ❍ /helpconnect: List available commands that can be used remotely
+ ❉ /connect: Connects to chat (Can be done in a group by /connect or /connect <chat id> in PM)
+ ❉ /connection: List connected chats
+ ❉ /disconnect: Disconnect from a chat
+ ❉ /helpconnect: List available commands that can be used remotely
 
 *Admin only:*
- ❍ /allowconnect <yes/no>: allow a user to connect to a chat
+ ❉ /allowconnect <yes/no>: allow a user to connect to a chat
 """
 
 CONNECT_CHAT_HANDLER = CommandHandler(
