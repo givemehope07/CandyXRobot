caab82fa48c00320d0b569908df2281fa9a66b26
ᴅᴇᴠᴀɴsʜ-ʙᴏᴛs <125127646+Devansh-Bots@users.noreply.github.com>
2023-06-17 12:22:19 +0530

    Update group.py

--- FallenRobot/modules/group.py
+++ FallenRobot/modules/group.py
@@ -1,9 +1,9 @@
 __help__ = """
- ❍ /setgtitle <newtitle>*:* Sets new chat title in your group.
- ❍ /setgpic*:* As a reply to file or photo to set group profile pic!
- ❍ /delgpic*:* Same as above but to remove group profile pic.
- ❍ /setsticker*:* As a reply to some sticker to set it as group sticker set!
- ❍ /setdescription <description>*:* Sets new chat description in group.
+ ❉ /setgtitle <newtitle>*:* Sets new chat title in your group.
+ ❉ /setgpic*:* As a reply to file or photo to set group profile pic!
+ ❉ /delgpic*:* Same as above but to remove group profile pic.
+ ❉ /setsticker*:* As a reply to some sticker to set it as group sticker set!
+ ❉ /setdescription <description>*:* Sets new chat description in group.
 """
 
-__mod_name__ = "Gʀᴏᴜᴘ"
+__mod_name__ = "𝐆ʀᴏᴜᴘ"
