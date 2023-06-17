ed198b7b1b6bafd9d7355215e077ba0df559db3e
ᴅᴇᴠᴀɴsʜ-ʙᴏᴛs <125127646+Devansh-Bots@users.noreply.github.com>
2023-06-17 12:53:31 +0530

    Update math.py

--- FallenRobot/modules/math.py
+++ FallenRobot/modules/math.py
@@ -101,28 +101,28 @@ def log(update: Update, context: CallbackContext):
 __help__ = """
 *MATHS*
 Solves complex math problems using https://newton.now.sh
-❍ /math*:* Math `/math 2^2+2(2)`
-❍ /factor*:* Factor `/factor x^2 + 2x`
-❍ /derive*:* Derive `/derive x^2+2x`
-❍ /integrate*:* Integrate `/integrate x^2+2x`
-❍ /zeroes*:* Find 0's `/zeroes x^2+2x`
-❍ /tangent*:* Find Tangent `/tangent 2lx^3`
-❍ /area*:* Area Under Curve `/area 2:4lx^3`
-❍ /cos*:* Cosine `/cos pi`
-❍ /sin*:* Sine `/sin 0`
-❍ /tan*:* Tangent `/tan 0`
-❍ /arccos*:* Inverse Cosine `/arccos 1`
-❍ /arcsin*:* Inverse Sine `/arcsin 0`
-❍ /arctan*:* Inverse Tangent `/arctan 0`
-❍ /abs*:* Absolute Value `/abs -1`
-❍ /log*:* Logarithm `/log 2l8`
+❉ /math*:* Math `/math 2^2+2(2)`
+❉ /factor*:* Factor `/factor x^2 + 2x`
+❉ /derive*:* Derive `/derive x^2+2x`
+❉ /integrate*:* Integrate `/integrate x^2+2x`
+❉ /zeroes*:* Find 0's `/zeroes x^2+2x`
+❉ /tangent*:* Find Tangent `/tangent 2lx^3`
+❉ /area*:* Area Under Curve `/area 2:4lx^3`
+❉ /cos*:* Cosine `/cos pi`
+❉ /sin*:* Sine `/sin 0`
+❉ /tan*:* Tangent `/tan 0`
+❉ /arccos*:* Inverse Cosine `/arccos 1`
+❉ /arcsin*:* Inverse Sine `/arcsin 0`
+❉ /arctan*:* Inverse Tangent `/arctan 0`
+❉ /abs*:* Absolute Value `/abs -1`
+❉ /log*:* Logarithm `/log 2l8`
 
 _Keep in mind_: To find the tangent line of a function at a certain x value, send the request as c|f(x) where c is the given x value and f(x) is the function expression, the separator is a vertical bar '|'. See the table above for an example request.
 To find the area under a function, send the request as c:d|f(x) where c is the starting x value, d is the ending x value, and f(x) is the function under which you want the curve between the two x values.
 To compute fractions, enter expressions as numerator(over)denominator. For example, to process 2/4 you must send in your expression as 2(over)4. The result expression will be in standard math notation (1/2, 3/4).
 """
 
-__mod_name__ = "Mᴀᴛʜs"
+__mod_name__ = "𝐌ᴀᴛʜs"
 
 SIMPLIFY_HANDLER = DisableAbleCommandHandler("math", simplify, run_async=True)
 FACTOR_HANDLER = DisableAbleCommandHandler("factor", factor, run_async=True)
