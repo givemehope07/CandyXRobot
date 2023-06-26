6faf406f620d3fc26a896f2b8e76fe869f552cb1
ɴᴏᴏʙ - ᴍᴜᴋᴇsʜ <108251605+Noob-mukesh@users.noreply.github.com>
2023-06-26 23:36:57 +0530

    Update __main__.py
    Fix

--- FallenRobot/__main__.py
+++ FallenRobot/__main__.py
@@ -907,7 +907,7 @@ def main():
     dispatcher.add_handler(help_callback_handler)
     dispatcher.add_handler(settings_callback_handler)
     dispatcher.add_handler(migrate_handler)
-
+    dispatcher.add_handler(Music_callback_handler)
     dispatcher.add_error_handler(error_callback)
 
     LOGGER.info("Using long polling.")
