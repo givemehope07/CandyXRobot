d64769fd0e19a862afddc9759be701b266df9225
ᴅᴇᴠᴀɴsʜ-ʙᴏᴛs <125127646+Devansh-Bots@users.noreply.github.com>
2023-06-26 22:54:51 +0530

    Update dev.py

--- FallenRobot/modules/dev.py
+++ FallenRobot/modules/dev.py
@@ -88,6 +88,6 @@ dispatcher.add_handler(LEAVE_HANDLER)
 dispatcher.add_handler(GITPULL_HANDLER)
 dispatcher.add_handler(RESTART_HANDLER)
 
-__mod_name__ = "𝐃ᴇᴠs"
+__mod_name__ = "𝐃ᴇᴠ"
 
 __handlers__ = [LEAVE_HANDLER, GITPULL_HANDLER, RESTART_HANDLER, ALLOWGROUPS_HANDLER]
